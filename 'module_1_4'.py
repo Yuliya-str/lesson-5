9fdcb74be3e326b691bb4b55c89587621429314a
Yuliya-str <gliceriya@yandex.ru>
2024-09-01 21:04:24 +0300

    Add files via upload

---
+++ 'module_1_4'.py
@@ -0,0 +1,7 @@
+my_string='Какой хороший день'
+print(my_string.upper())
+print(my_string.lower())
+print('Какой хороший день'.replace(" ", ""))
+print(my_string[0:1])
+print(my_string[-1])
+print(len(my_string))
